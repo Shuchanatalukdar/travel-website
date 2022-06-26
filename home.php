bcbea33daecff71626eb8d6fe287e09a7184b564
Shuchana Talukdar <shuchanatalukdaricse@gmail.com>
2022-06-26 11:19:11 +0530

    Initial commit

---
+++ home.php
@@ -0,0 +1,283 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>home</title>
+
+   <!-- swiper css link  -->
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<!-- header section starts  -->
+
+<section class="header">
+   <img src="./images/airplane.png" alt="" style="width:60px">
+   <a href="home.php" class="logo">Your TravelBuddy</a>
+
+   <nav class="navbar">
+      <a href="home.php">home</a>
+      <a href="about.php">about</a>
+      <a href="package.php">package</a>
+      <a href="book.php">book</a>
+   </nav>
+
+   <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+<!-- header section ends -->
+
+<!-- home section starts  -->
+
+<section class="home">
+
+   <div class="swiper home-slider">
+
+      <div class="swiper-wrapper">
+         
+         <div class="swiper-slide slide" style="background:url(images/bg1.jpg) no-repeat">
+            <div class="content">
+               <span>The journey of a thousand miles begins with a single step.</span>
+               <!-- <h3>MAKE YOUR DREAM OF TRAVELLING AROUND THE WORLD YOUR REALITY!</h3> -->
+               <a href="package.php" class="btn">Get To Know More</a>
+            </div>
+         </div>
+
+         <div class="swiper-slide slide" style="background:url(images/bg2.jpg) no-repeat;">
+            <div class="content">
+               <span>I haven't been everywhere, but it's on my list</span>
+               <!-- <h3>TRAVEL WITH US TO GET THE BEST OFFERS YOU EVER RECEIVED!</h3> -->
+               <a href="package.php" class="btn">Get To Know More</a>
+            </div>
+         </div>
+
+         <div class="swiper-slide slide" style="background:url(images/bg3.jpg) no-repeat">
+            <div class="content">
+               <span>Have stories to tell not stuff to show</span>
+               <!-- <h3>YOU ARE LITERALLY ONE CLICK AWAY FROM MAKING THE BEST ADVENTURES OF YOUR LIFE!</h3> -->
+               <a href="package.php" class="btn">Get To Know More</a>
+            </div>
+         </div>
+         
+      </div>
+
+      <div class="swiper-button-next"></div>
+      <div class="swiper-button-prev"></div>
+
+   </div>
+
+</section>
+
+<!-- home section ends -->
+
+<!-- services section starts  -->
+
+<section class="services">
+
+   <h1 class="heading-title"> SERVICES OFFERED </h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <img src="images/icon-1.png" alt="">
+         <h3>Experience</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/icon-2.png" alt="">
+         <h3>Your TravelBuddy</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/icon-3.png" alt="">
+         <h3>trekking</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/icon-4.png" alt="">
+         <h3>Best Food</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/icon-5.png" alt="">
+         <h3>Boating</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/icon-6.png" alt="">
+         <h3>camping</h3>
+      </div>
+
+   </div>
+
+</section>
+
+<!-- services section ends -->
+
+<!-- home about section starts  -->
+
+<section class="home-about">
+
+   <div class="image">
+      <img src="images/girl.svg" alt="">
+   </div>
+
+   <div class="content">
+      <h3>GET TO KNOW ABOUT US</h3>
+      <p>Your TravelBuddy is known for its collection of reviews and articles. Believe it or not, you can actually create an entire vacation package on the website, it is considered one of the best in the business. Key attractions are : Best place to get a general idea of things to do in a particular area, booking options are solid (for casual or luxury travel), also provides car and bike rental options, filters are easy to manipulate and tabs are simple to search through and prices are competitive during heavy travel times.</p>
+      <a href="about.php" class="btn">read more</a>
+   </div>
+
+</section>
+
+<!-- home about section ends -->
+
+<!-- home packages section starts  -->
+
+<section class="home-packages">
+
+   <h1 class="heading-title"> BEST PACKAGES EVER! </h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <div class="image">
+            <img src="images/card1.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>EXPLORE WITH US</h3>
+            <h3>NETHERLAND</h3>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/card2.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>EXPLORE WITH US</h3>
+            <h3>GERMANY</h3>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+      
+      <div class="box">
+         <div class="image">
+            <img src="images/card3.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>EXPLORE WITH US</h3>
+            <h3>FRANCE</h3>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+   </div>
+
+   <div class="load-more"> <a href="package.php" class="btn">load more</a> </div>
+
+</section>
+
+<!-- home packages section ends -->
+
+<!-- home offer section starts  -->
+
+<section class="home-offer">
+   <div class="content">
+      <h3>UPTO 60% OFF</h3>
+      <p>Searching for the best deals? We are here with the greatest offer you have ever seen. Then what are you waiting for?. Join with us to create the best adventures of your life. Happy Journey!.</p>
+      <a href="book.php" class="btn">PLAN YOUR JOURNEY NOW</a>
+   </div>
+</section>
+
+<!-- home offer section ends -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- footer section starts  -->
+
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+         <a href="package.php"> <i class="fas fa-angle-right"></i> package</a>
+         <a href="book.php"> <i class="fas fa-angle-right"></i> book</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="#"> <i class="fas fa-angle-right"></i> Tell us about your queries </a>
+         <a href="#"> <i class="fas fa-angle-right"></i> Get to know about us</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> Security and much more</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> Terms and conditions applied</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <a href="#"> <i class="fas fa-phone"></i> +123-456-7890 </a>
+         <a href="#"> <i class="fas fa-phone"></i> +111-222-3333 </a>
+         <a href="#"> <i class="fas fa-envelope"></i> xxxxxxx@gmail.com </a>
+         <a href="#"> <i class="fas fa-map"></i> Asansol, India</a>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </div>
+
+   <div class="credit"> created by <span>IT TEAM FROM AEC</span> | all rights reserved! </div>
+
+</section>
+
+<!-- footer section ends -->
+
+
+
+
+
+
+
+
+
+<!-- swiper js link  -->
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
